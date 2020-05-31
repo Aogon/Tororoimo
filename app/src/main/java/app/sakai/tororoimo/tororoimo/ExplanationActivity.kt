865677356912b90539001bb710a2c6f754202f5a
Aogon <aoi_sakai@icloud.com>
2020-05-31 13:54:22 +0900

    [add]Activityの追加

---
+++ app/src/main/java/app/sakai/tororoimo/tororoimo/ExplanationActivity.kt
@@ -0,0 +1,12 @@
+package app.sakai.tororoimo.tororoimo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExplanationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explanation)
+    }
+}
