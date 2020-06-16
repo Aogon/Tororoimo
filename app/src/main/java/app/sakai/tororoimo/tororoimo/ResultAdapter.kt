0f443cc84e5aa0412d03c94b814aefe6d850c03f
Aogon <aoi_sakai@icloud.com>
2020-06-16 21:11:21 +0900

    [add]CustomAdapterの作成

---
+++ app/src/main/java/app/sakai/tororoimo/tororoimo/ResultAdapter.kt
@@ -0,0 +1,40 @@
+package app.sakai.tororoimo.tororoimo
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
+import kotlinx.android.synthetic.main.list_item.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ResultAdapter (
+    private val context: Context,
+    private var resultList: OrderedRealmCollection<Results>?,
+    private var autoUpdate: Boolean
+) :
+        RealmRecyclerViewAdapter<Results, ResultAdapter.ResultViewHolder>(resultList, autoUpdate) {
+    override fun getItemCount(): Int = resultList?.size ?: 0
+    override fun onBindViewHolder(holder: ResultAdapter.ResultViewHolder, position: Int) {
+        val result: Results = resultList?.get(position) ?: return
+        holder.dateTextView.text = SimpleDateFormat("yyyy年MM月dd日", Locale.JAPANESE).format(result.date)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ResultAdapter.ResultViewHolder {
+        val v = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        return ResultViewHolder(v)
+    }
+
+    class ResultViewHolder(view: View) :RecyclerView.ViewHolder(view) {
+        val dateTextView: TextView = view.dateTextView
+        val resultTextView: TextView = view.resultTextView
+    }
+}
+
